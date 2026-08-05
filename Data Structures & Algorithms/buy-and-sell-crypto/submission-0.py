223c6679e38975d14b8c8c7297dc23bdce415c0b
vedanti-akare <vedantiakare03@gmail.com>
2026-08-05 11:58:51 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price=prices[0]
+        max_profit=0
+        for price in prices:
+            min_price=min(price,min_price)
+            profit=price-min_price
+            max_profit=max(profit,max_profit)
+        return max_profit
